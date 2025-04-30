d191ce510d937c65b1db0bf4e7467459c8df511a
Chris Landers <chrislanders700@gmail.com>
2025-04-30 16:30:50 -0700

    Revert "add log, also add themes"
    This reverts commit 3c0fd26895e1d5fed0b82d77260b40ef77e5a3c8.

--- scripts/built/formatPage.js
+++ scripts/built/formatPage.js
@@ -1,7 +1,4 @@
 var topbarHeight = document.getElementById("topBar").clientHeight;
-var tbHeight = document.getElementById("themeButton").clientHeight;
 document.getElementById("dashboard").style.marginTop = topbarHeight + "px";
 document.getElementById("dashboard").style.top = topbarHeight + "px";
-document.getElementById("themeButton").style.top = ((topbarHeight / 2) - (tbHeight / 2)).toString() + "px";
-console.log(((topbarHeight / 2) - (tbHeight / 2)).toString());
 //# sourceMappingURL=formatPage.js.map
